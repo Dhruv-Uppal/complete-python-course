e4d3cf465154df3ce200ab99e0bbb19a432f5487
Dhruv-Uppal <141750918+Dhruv-Uppal@users.noreply.github.com>
2023-08-27 19:15:06 +0530

    Update incomplete_app.py

--- course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
+++ course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
@@ -5,6 +5,7 @@ movies = []
 
 
 # You may want to create a function for this code
+def add_movie():
 title = input("Enter the movie title: ")
 director = input("Enter the movie director: ")
 year = input("Enter the movie release year: ")
@@ -15,6 +16,26 @@ movies.append({
     'year': year
 })
 
+def show_movie():
+    for movie in movies:
+        print_movie(movie)
+
+def print_movie(movie):
+    print(f"title:{movie['Title']}")
+    print(f"director:{movie['Director']}")
+    print(f"year:{movie['year']}")
+
+def find_movie():
+    Search_Title=input("Enter the name of the movie you are looking for:")
+    for movie in movies:
+        if movie["title"]==Search_Title:
+            print_movie(movie)
+
+user_options={
+    'a'=add_movie,
+    'l'=show_movie,
+    'f'=find_movie
+}
 
 # Create other functions for:
 #   - listing movies
@@ -22,18 +43,21 @@ movies.append({
 
 
 # And another function here for the user menu
-selection = input(MENU_PROMPT)
-while selection != 'q':
-    if selection == "a":
-        pass
-    elif selection == "l":
-        pass
-    elif selection == "f":
-        pass
-    else:
-        print('Unknown command. Please try again.')
-
+def menu():
     selection = input(MENU_PROMPT)
+        while selection != 'q':
+            if selection == "a":
+                pass
+            elif selection == "l":
+                pass
+            elif selection == "f":
+                pass
+            else:
+               print('Unknown command. Please try again.')
 
+            selection = input(MENU_PROMPT)
+
+
+menu()
 
 # Remember to run the user menu function at the end!
